680c7d7dcf0ea79f242774b960e7587c47fcf013
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-02-01 11:09:43 +0700

    Memperbaiki Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,11 @@
-import React from "react";
 import {
     Navbar,
     Typography,
-    Avatar,
 } from "@material-tailwind/react";
-import { FaSun } from "react-icons/fa";
+import { ProfileMenu } from "./ProfileMenu";
 
 
 function Navigasi() {
-    const [openNav, setOpenNav] = React.useState(false);
-
-    React.useEffect(() => {
-        window.addEventListener(
-            "resize",
-            () => window.innerWidth >= 960 && setOpenNav(false),
-        );
-    }, []);
-
     return (
         <div className="-m-6 max-h-[768px] w-[calc(100%+48px)]">
             <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
@@ -31,7 +20,7 @@ function Navigasi() {
                     </Typography>
                     <div className="flex items-center gap-4">
                         <div className="flex items-center gap-x-1">
-                            <Avatar src="https://docs.material-tailwind.com/img/face-2.jpg" alt="avatar" />
+                            <ProfileMenu />
                         </div>
 
                     </div>
